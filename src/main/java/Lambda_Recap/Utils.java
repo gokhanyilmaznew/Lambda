3a7c6d49fe3a2b04827b18a6a1b2f117a86260d9
gokhanyilmaznew <gokhan.isbirlikleri@gmail.com>
2023-05-25 14:06:22 +0300

    LambdaRecap

---
+++ src/main/java/Lambda_Recap/Utils.java
@@ -0,0 +1,18 @@
+package Lambda_Recap;
+
+public class Utils {
+    public static void yazdir(Object a) {
+        System.out.print(a + " ");
+        //Butun data type'larini yazdirabilelim diye Object yaptik.
+    }
+
+    public static int kareBul(int a) {
+        return a * a;
+    }
+
+    public static int kupBul(int a) {
+        return a * a * a;
+    }
+
+
+}
